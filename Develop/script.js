6beedc54fd880cc5914c146fe349d2a1d8a3b412
YakiniA <yakinilatha09@gmail.com>
2020-06-23 17:29:11 -0600

    added color depending on time

--- Develop/script.js
+++ Develop/script.js
@@ -9,14 +9,33 @@ function WorkScheduler() {
 
       var hours = ["9 AM.", "10 AM.", "11 AM.", "12 PM.", "1 PM.", "2 PM.", "3 PM.", "4 PM.",
         "5 PM."];
-      
+      var k=9;
       for (i = 0; i < hours.length; i++) {
-        var row = $("<div>").addClass("row");
+      
         var hourCol = $("<textarea>").text(hours[i]).addClass("hour");
         var inputCol = $("<input>").attr("placeholder", "Enter Your Notes here").addClass("inputText time-block");
-        var saveBtn = $("<button>").addClass("btn btn-primary saveBtn").text("save");
-        $(row).append(hourCol).append(inputCol).append(saveBtn);
-        $(".container").append(row);
+     
+        var hour = moment().hour();;
+        console.log(hour);
+
+        if(k!=null && k<18){
+        if (k > hour) {
+          inputCol.attr("class", "future");
+        } else if (k < hour) {
+          inputCol.attr("class", "past");
+        } else {
+          inputCol.attr("class", "present");
+        }
+        k++;
+
+      }
+            
+      var row = $("<div>").addClass("row");
+      // var row1 = $("#row");
+      var saveBtn = $("<button>").addClass("btn btn-primary saveBtn").text("save");
+      $(row).append(hourCol).append(inputCol).append(saveBtn);
+      $(".container").append(row);
+
       }
     });
 }
