872179c814db6d4f0f77bd57e203f29f9f6222a8
YakiniA <yakinilatha09@gmail.com>
2020-06-24 22:51:02 -0600

    developed full code

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,3 @@
-var schedulerText;
-var schedulerIndex;
 function WorkScheduler() {
 
     $(document).ready(function () {
@@ -43,70 +41,78 @@ function WorkScheduler() {
 
       }
 
-      var input0 = $(".inputVal-0");
-      var input1 = $(".inputVal-1");
-      var input2 = $(".inputVal-2");
-      var input3 = $(".inputVal-3");
-      var input4 = $(".inputVal-4");
-      var input5 = $(".inputVal-5");
-      var input6 = $(".inputVal-6");
-      var input7 = $(".inputVal-7");
-      var input8 = $(".inputVal-8");
-
       $(".saveBtn").on("click",function (event) {
       
         event.preventDefault();
         console.log("Inside Save");
         var val = $(this).siblings("input").val().trim();
-        var valIndex = $(this).siblings("textarea").attr("data-value");
-      
-        console.log("Val" + val);
-        console.log("valIndex" + valIndex);
-        saveDetails(val, valIndex);
+        var valIndex = parseInt($(this).siblings("textarea").attr("data-value"));
+
+        if(valIndex===0){
+                var input0 =val;
+                localStorage.setItem("Text0",input0);
+           } else if(valIndex===1){
+                var input1=val;
+                localStorage.setItem("Text1",input1);
+           } else if(valIndex===2){
+                var input2=val;
+                localStorage.setItem("Text2",input2);
+           } else if(valIndex===3){
+                var input3=val;
+                localStorage.setItem("Text3",input3);
+           }else if(valIndex===4){
+                var input4=val;
+                localStorage.setItem("Text4",input4);
+           }else if(valIndex===5){
+                var input5=val;
+                localStorage.setItem("Text5",input5);
+           }else if(valIndex===6){
+                var input6=val;
+                localStorage.setItem("Text6",input6);
+           }else if(valIndex===7){
+                var input7=val;
+                localStorage.setItem("Text7",input7);
+           }else if(valIndex===8){
+                var input8=val;
+                localStorage.setItem("Text8",input8);
+           }
+          
   });
+
+  var value0 = localStorage.getItem("Text0");
+  $(".inputVal-0").val(value0);
+
+  var value1 = localStorage.getItem("Text1");
+  $(".inputVal-1").val(value1);
+
+  var value2 = localStorage.getItem("Text2");
+  $(".inputVal-2").val(value2);
+
+  var value3 = localStorage.getItem("Text3");
+  $(".inputVal-3").val(value3);
+
+  var value4= localStorage.getItem("Text4");
+  $(".inputVal-4").val(value4);
+
+  var value5 = localStorage.getItem("Text5");
+  $(".inputVal-5").val(value5);
+
+  var value6 = localStorage.getItem("Text6");
+  $(".inputVal-6").val(value6);
+
+  var value7 = localStorage.getItem("Text7");
+  $(".inputVal-7").val(value7);
+
+  var value8 = localStorage.getItem("Text8");
+  $(".inputVal-8").val(value8);
+
 });
 }
-
-WorkScheduler();
-displayItem();
-
-function saveDetails(text, index) {
-
-      if (localStorage.getItem("schedulerText") && localStorage.getItem("schedulerIndex")) {
-        schedulerText = JSON.parse(localStorage.getItem("schedulerText"));
-        schedulerIndex = JSON.parse(localStorage.getItem("schedulerIndex"));
         
-      } else {
-        schedulerText = [];
-        schedulerIndex = [];
-      }
+WorkScheduler();
 
-      schedulerText.push(text);
-      schedulerIndex.push(index);
 
-      localStorage.setItem("schedulerText", JSON.stringify(schedulerText));
-      localStorage.setItem("schedulerIndex", JSON.stringify(schedulerIndex));
-     
-  }
-
-   function displayItem() {
-        console.log("I am inside dispalyitem");
-        var finalSchedulerText = JSON.parse(localStorage.getItem("schedulerText"));
-        var finalSchedulerIndex = JSON.parse(localStorage.getItem("schedulerIndex"));
-        console.log(finalSchedulerText);
-        console.log(finalSchedulerIndex);
-        if (finalSchedulerIndex != null) {
-          for (var i = 0; i < finalSchedulerIndex.length; i++) {
-            debugger
-            console.log("FinalSchedulerIndexValue" + finalSchedulerIndex[i]);
-            // var booleanResp = $(".inputVal").attr("data-value") === parseInt(finalSchedulerIndex[i]);
-            // if(booleanResp){
-                // var inputFinal =  $(".inputVal-"+finalSchedulerIndex[i]).val(finalSchedulerText[i]);
-                // var inputFinal = finalSchedulerText[i];
-                schedulerIndex[i] = finalSchedulerIndex[i];
-                schedulerText[i] = finalSchedulerText[i];        }
-        }
-      }
+      
   
 
 
